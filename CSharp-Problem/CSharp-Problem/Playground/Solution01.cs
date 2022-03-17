df7cfaaf7f5d8b6bf59cf92004751b9c6fa05f7f
lebrancconvas <lebrancconvas@gmail.com>
2022-03-17 20:39:27 +0700

    :tada: Init.

---
+++ CSharp-Problem/CSharp-Problem/Playground/Solution01.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace CSharp_Problem.Playground
+{
+    public class Solution01
+    {
+        public Solution01()
+        {
+            String numberterminal;
+            String numberconcat = "";
+
+            Console.WriteLine("Enter Your Number: ");
+            numberterminal = Console.ReadLine();
+
+            int number = Convert.ToInt16(numberterminal);
+
+            for (int i = 1; i <= number; i++)
+            {
+                numberconcat += i;
+            }
+
+            Console.WriteLine(numberconcat);
+        }
+    }
+}
---
+++ CSharp-Problem/CSharp-Problem/Playground/ValueReferenceType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace CSharp_Problem.Playground
+{
+    public class ValueReferenceType
+    {
+        public ValueReferenceType()
+        {
+            // Value Data Type (Ex. Integer)
+            int x = 1;
+            int y = 1;
+            int z = y;
+            Console.WriteLine(x == y); // True
+            Console.WriteLine(x == z); // True
+            Console.WriteLine(y == z); // True 
+
+            // Reference Data Type (Ex. Array)
+            int[] a = new int[] { 1, 2, 3 };
+            int[] b = new int[] { 1, 2, 3 };
+            int[] c = b;
+            Console.WriteLine(a == b); // False 
+            Console.WriteLine(a == c); // False 
+            Console.WriteLine(b == c); // True 
+        }
+    }
+}
